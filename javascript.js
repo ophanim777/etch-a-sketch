2bd6f97e5996b74935a0862992fb588b278271cc
ophanim777 <rikyjo49@gmail.com>
2025-11-03 08:11:30 +0700

    add new function

--- javascript.js
+++ javascript.js
@@ -1,25 +1,27 @@
 const container = document.querySelector('.container');
 const newGridBtn = document.getElementById('new-grid');
 const containerSize = 960;
+ 
+ function createGrid(size) {
+      container.innerHTML = '';
+      const squareSize = containerSize / size;
 
-const gridSize = 16;
+      for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
-function createGrid(size) {
-  const squareSize = 600 / size; // fits squares perfectly inside container
-  for (let i = 0; i < size * size; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
-  ;
-   square.addEventListener('mouseover', () => {
-      square.style.backgroundColor = 'black';
-    });
-    container.appendChild(square);
-  }
-}
+        square.addEventListener('mouseover', () => {
+          square.style.backgroundColor = 'black';
+        });
 
-createGrid(gridSize);
+        container.appendChild(square);
+      }
+    }
+
+    
+    createGrid(16);
 
 const randomColor = () => `hsl(${Math.random() * 360}, 100%, 50%)`;
 square.addEventListener('mouseover', () => {
