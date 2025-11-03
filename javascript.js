a3af7be8f27b60252475d6c0b0e8f589250be053
ophanim777 <rikyjo49@gmail.com>
2025-11-03 07:20:12 +0700

    add function create grid

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
 const container = document.querySelector('.container');
 const gridSize = 16;
+function createGrid(size) {
+  const squareSize = 600 / size; // fits squares perfectly inside container
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+  }};
