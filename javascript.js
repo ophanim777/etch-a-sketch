81d8ad0c972b552aa24d2aece31600087b66dcc7
ophanim777 <rikyjo49@gmail.com>
2025-11-03 07:22:39 +0700

    add hover effect

--- javascript.js
+++ javascript.js
@@ -8,3 +8,6 @@ function createGrid(size) {
     square.style.width = `${squareSize}px`;
     square.style.height = `${squareSize}px`;
   }};
+   square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
